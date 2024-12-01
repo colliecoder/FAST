2a576ab89f7e5e25b3e886e1559adde68d8f9dc6
Kiersten Campbell <kiersten@kierstens-mbp.home>
2024-12-01 16:53:49 -0500

    initial commit to heroku

--- server.py
+++ server.py
@@ -492,10 +492,6 @@ def analyze():
 def helloworld():
     return "Hello World!"
 
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Get the PORT from the environment or default to 5000
     app.run(host="0.0.0.0", port=port)
